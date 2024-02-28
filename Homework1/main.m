8108cf5b2bd45fad988c84ea454ae48aabd6be5f
radamin <radamin@yandex.ru>
2024-02-29 01:36:26 +0300

    Первое домашнее задание по курсу языка программирования Objective-C

--- Homework1/main.m
+++ Homework1/main.m
@@ -9,8 +9,58 @@
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+        // Task 1.
+        // Решить квадратные уравнения:
+        // x^2 – 8x + 12 = 0,
+        // 12x^2 – 4x + 2 = 0,
+        // x^2 – 100x - 2 = 0.
+        // Заменять константы в коде
+                
+        float a, b, c;
+        NSLog(@"Введите коэффициент a: ");
+        scanf("%f", &a);
+        NSLog(@"Введите коэффициент b: ");
+        scanf("%f", &b);
+        NSLog(@"Введите коэффициент c: ");
+        scanf("%f", &c);
+                
+        if (a == 0) {
+        NSLog(@"Уравнение не является квадратным");
+        } else {
+            
+        float discriminant = b * b - 4 * a * c;
+                            
+        if (discriminant > 0) {
+        float x1 = (-b + sqrt(discriminant)) / (2 * a);
+        float x2 = (-b - sqrt(discriminant)) / (2 * a);
+        NSLog(@"Корни уравнения: x1 = %f, x2 = %f", x1, x2);
+        } else if (discriminant == 0) {
+        float x = -b / (2 * a);
+        NSLog(@"Уравнение имеет один корень: x = %f", x);
+        } else {
+        NSLog(@"Корней у уравнения нет");
+        }
+        }
+                
+        // Task 2.
+        // Создать программу, которая находит большее число среди 3-х чисел.
+                
+        float num1, num2, num3;
+        NSLog(@"Введите первое число: ");
+        scanf("%f", &num1);
+        NSLog(@"Введите второе число: ");
+        scanf("%f", &num2);
+        NSLog(@"Введите третье число: ");
+        scanf("%f", &num3);
+                
+        if (num1 >= num2 && num1 >= num3) {
+        NSLog(@"Наибольшее число: %f", num1);
+        } else if (num2 >= num1 && num2 >= num3) {
+        NSLog(@"Наибольшее число: %f", num2);
+        } else {
+        NSLog(@"Наибольшее число: %f", num3);
+        }
+
     }
     return 0;
 }
