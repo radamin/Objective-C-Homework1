feec48578868ebfdf4b010f319eb91943afd5688
radamin <radamin@yandex.ru>
2024-02-29 00:54:44 +0300

    Initial Commit

---
+++ Homework1/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Homework1
+//
+//  Created by Рамазан Даминов on 29.02.2024.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
